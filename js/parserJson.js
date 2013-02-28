bd5dfc27286c1ad05e236227978bab25f962833e
wirthc <c.wirth@live.fr>
2013-02-28 15:48:08 +0100

    Ajout d'un parser json. Il suffit d'implémenter une méthode showData pour accéder aux données

---
+++ js/parserJson.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+
+var data;
+
+
+$(document).ready(function ()
+{
+    $.ajax({
+    type: "GET",
+    url: "data/data2.json",
+    dataType: "json",
+    success: showData,
+	error: errorfunction
+  });
+   
+});
+
+function errorfunction(json)
+{
+	alert("Erreur, impossible de récupérer les données.");
+	
+}
+
+function getData() {
+    return data;
+}
