656d6635451412ddb4d08a4c106cbc503ef83475
wirthc <c.wirth@live.fr>
2013-03-06 09:32:02 +0100

    Correction suite aux précédents commits

--- js/parserJson.js
+++ js/parserJson.js
@@ -1,6 +1,15 @@
 // JavaScript Document
 
+function parser(url) {
+	$.ajax({
+    type: "GET",
+    url: url,
+    dataType: "json",
+    success: showData,
+	error: errorfunction
+	});
 
+}
 
 function parser(){
     $.ajax({
@@ -15,7 +24,7 @@ function parser(){
 
 function errorfunction(json)
 {
-	alert("Erreur, impossible de récupérer les données.");
+	alert("Erreur, impossible de r&eacutecup&eacuterer les donn&eacutees.");
 	
 }
 
